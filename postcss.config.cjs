551f9491dfe78503eca4d649caea6323c8a6a4f6
AmuchanDesu <96105271+AmuchanDesu@users.noreply.github.com>
2024-09-20 22:05:16 +0700

    Add files via upload

---
+++ postcss.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',  // ปรับเส้นทางไฟล์ให้ตรงกับโครงสร้างโปรเจกต์ของคุณ
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',  // ปรับเส้นทางไฟล์ให้ตรงกับโครงสร้างโปรเจกต์ของคุณ
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
